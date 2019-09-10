9bc480475dc1e20b5e7805987a39a932bddd8acc
Semreg <vchabaniuk@yahoo.com>
2019-09-10 20:01:05 +0300

    Update cv folder

---
+++ 01/vlad_chabaniuk/1_regulyarnye_vyrazheniya/01_jsa/js/regexp.js
@@ -0,0 +1,13 @@
+
+
+// Два способа создания регулярного выражения
+// Методы regexp
+// flags
+// dot pattern
+// quantity
+// character class  pattern, negate character class, ranges
+// grouping pattern, alternation
+// lookahead  ?=, ?! */
+// word boundary \b, \B
+
+var st = "Is this This?";
