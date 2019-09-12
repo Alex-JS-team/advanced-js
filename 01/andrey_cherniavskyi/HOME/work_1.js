92e515fb719717208737adb5ed7feb3b854596d6
Andrii Cherniavskyi <andruhach07@gmail.com>
2019-09-12 23:58:11 +0300

    less_1_HW_1_AC

--- 01/andrey_cherniavskyi/HOME/work_1.js
+++ 01/andrey_cherniavskyi/HOME/work_1.js
@@ -9,3 +9,17 @@
 let st = `063-111-1234
 (333) 456-8768
 4446575547`;
+
+
+function format(number) {
+    number = number.replace(/[^\d]/g, "");
+    number = number.match(/\d{1,10}/g);
+
+    let newNumber = [];
+    number.forEach (function(i) {
+        newNumber.push(i.replace(/(\d{3})(\d{3})(\d{4})/, "($1) $2 $3"));
+    });
+    return newNumber;
+}
+
+console.log(format(newNumber));
