8ffadfbfd0696aed12ad0e7fd130db9b9f01837b
Andrii Cherniavskyi <andruhach07@gmail.com>
2019-09-14 19:48:09 +0300

    HW_friends_list

---
+++ 02/2_materialy_k_uroku/02_jsa/LABS/test_context/spec/context.spec.js
@@ -0,0 +1,15 @@
+describe('Investigate context of function', function(){
+    
+  it('should return 5', () => {
+      expect(5).toBe(5)
+  }) 
+})
+
+
+
+describe("Test function context", () => {
+  it('should  return  success', () => {
+    expect(incr()).toBe("success");
+ })
+})
+
