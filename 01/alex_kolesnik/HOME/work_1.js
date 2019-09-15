1741f594fec665cca472668e98483e816325cfc1
Alex Kolesnik <alekseyk.maxs@gmail.com>
2019-09-15 18:51:48 +0300

    work_1 is done

--- 01/alex_kolesnik/HOME/work_1.js
+++ 01/alex_kolesnik/HOME/work_1.js
@@ -7,4 +7,18 @@
 */
 let st = `063-111-1234
 (333) 456-8768
-4446575547`;f
+4446575547`;
+
+function format(phone) {
+    phone = phone.replace(/[^\d]/g, "");
+    phone = phone.match(/.{1,10}/g);
+
+    let newPhone= [];
+    phone.forEach (function(i) {
+        newPhone.push(i.replace(/(\d{3})(\d{3})(\d{4})/, "($1) $2 $3"));
+    });
+    return newPhone;
+}
+
+var result = format(st);
+result.forEach(number => console.log(number))
