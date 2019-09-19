cfc4a71dbd4c29107ec33017945ebeca2eef55fd
Alex Ordynski <dr.hikari@gmail.com>
2019-09-19 20:20:50 +0300

    [hot-fix] Add 04 homework folder.

---
+++ 04/HOME_WORK/index.js
@@ -0,0 +1,18 @@
+/*
+Реализовать иерархию классов синтаксисом ES 5 согласно 
+диаграммы классов приведенной в презентации home_oop
+*/
+
+
+
+
+// usage
+const cd = new CDProduct("Abbey road", 30, 45.20);
+console.log(cd.getInfo()); // Product: Abbey road, price: 30, length: 45.2
+
+const book = new BookProduct("React", 60, 125);
+console.log(book.getInfo()); //  Product: React, price: 60,  pages: 125
+
+
+
+
