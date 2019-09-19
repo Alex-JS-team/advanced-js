5cf04e8e9879b3a2b74c565736340c31fd3bb727
Alex Kolesnik <alekseyk.maxs@gmail.com>
2019-09-19 16:15:23 +0300

    1st done

--- 03/HOME_WORK/_/js/script.js
+++ 03/HOME_WORK/_/js/script.js
@@ -1,7 +1,7 @@
 (function(){
     this.assert = function(ob, st) {
         var li = document.createElement("li");
-        li.className = ob == true ? 'pass' : "fail";
+        li.className = ob ? 'pass' : "fail";
         var text = document.createTextNode(st);
         li.appendChild(text);
         document.getElementById("results").appendChild(li);
