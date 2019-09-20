2b92ab8f98000cf6e7194f225b30fefe9bd25156
Andrii Cherniavskyi <andruhach07@gmail.com>
2019-09-20 22:13:08 +0300

    classes hierarchy homework

--- 04/andrey_cherniavskyi/index.js
+++ 04/andrey_cherniavskyi/index.js
@@ -4,6 +4,27 @@
 */
 
 
+function ShopProduct(title, price) {
+    this.title = title;
+    this.price = price;
+
+    this.getTitle = () => this.title;
+    this.getPrice = () => this.price;
+}
+
+function CDProduct(title, price, playLength) {
+    ShopProduct.apply(this, arguments);
+    this.playLength = playLength;
+
+    this.getInfo = () => `Product: ${this.title}, price: ${this.price}, length: ${this.playLength}`;
+}
+function BookProduct(title, price, numPages) {
+    ShopProduct.apply(this, arguments);
+    this.numPages = numPages;
+
+    this.getInfo = () => `Product: ${this.title}, price: ${this.price}, pages: ${this.numPages}`;
+}
+
 
 
 // usage
@@ -12,7 +33,3 @@ console.log(cd.getInfo()); // Product: Abbey road, price: 30, length: 45.2
 
 const book = new BookProduct("React", 60, 125);
 console.log(book.getInfo()); //  Product: React, price: 60,  pages: 125
-
-
-
-
